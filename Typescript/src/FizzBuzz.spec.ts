e00adbd2390c44c77597c6dd3da0781e277b707a
Leto <leto4100@gmail.com>
2022-03-30 10:30:23 +0100

    Test passed - 1

--- Typescript/src/FizzBuzz.spec.ts
+++ Typescript/src/FizzBuzz.spec.ts
@@ -3,8 +3,8 @@ import { FizzBuzz } from './FizzBuzz';
 describe('FizzBuzz', () => {
     const fizzBuzz = new FizzBuzz();
     it('should return a number', () => {
-        expect(fizzBuzz.checkNumbers([1])).toBe(['1']);
-        expect(fizzBuzz.checkNumbers([16])).toBe(['16']);
-        expect(fizzBuzz.checkNumbers([34])).toBe(['34']);
+        expect(fizzBuzz.checkNumbers([1])).toStrictEqual(['1']);
+        expect(fizzBuzz.checkNumbers([16])).toStrictEqual(['16']);
+        expect(fizzBuzz.checkNumbers([34])).toStrictEqual(['34']);
     })
 })
--- Typescript/src/FizzBuzz.ts
+++ Typescript/src/FizzBuzz.ts
@@ -1,3 +1,10 @@
 export class FizzBuzz {
-    
+    public checkNumbers (numbers: number[]): string[] {
+
+        let result: string[] = numbers.map(number => {
+            return number.toString();
+        })
+        
+        return result;
+    }
 }
