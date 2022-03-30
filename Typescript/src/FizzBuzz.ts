da35668089b0f01affdc86753197814bd80f42cb
Leto <leto4100@gmail.com>
2022-03-30 10:48:45 +0100

    Test passed - 4

--- Typescript/src/FizzBuzz.ts
+++ Typescript/src/FizzBuzz.ts
@@ -1,14 +1,14 @@
 export class FizzBuzz {
   public checkNumbers(numbers: number[]): string[] {
     let result: string[] = numbers.map((number) => {
-      let numberResult: string = number.toString();
-      if(number % 3 === 0) {
-        numberResult = 'Fizz';
+      let numberResult: string = "";
+      if (number % 3 === 0) {
+        numberResult += "Fizz";
       }
-      if(number % 5 === 0) {
-        numberResult = 'Buzz';
+      if (number % 5 === 0) {
+        numberResult += "Buzz";
       }
-      return numberResult;
+      return numberResult != "" ? numberResult : number.toString();
     });
 
     return result;
